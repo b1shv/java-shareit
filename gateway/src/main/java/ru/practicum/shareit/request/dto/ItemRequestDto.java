9baf43db8589b14fb472bb1a82ca871d8265dbea
b1shv <a.balashovvv@gmail.com>
2023-07-05 13:51:34 +0300

    style: неиспользуемые импорты

--- gateway/src/main/java/ru/practicum/shareit/request/dto/ItemRequestDto.java
+++ gateway/src/main/java/ru/practicum/shareit/request/dto/ItemRequestDto.java
@@ -2,11 +2,8 @@ package ru.practicum.shareit.request.dto;
 
 import lombok.Builder;
 import lombok.Data;
-import ru.practicum.shareit.item.dto.ItemDto;
 
 import javax.validation.constraints.NotBlank;
-import java.time.LocalDateTime;
-import java.util.List;
 
 @Data
 @Builder
